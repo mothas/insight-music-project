3f20f5e01c7caaa5c10e7bef20262d2ed86491b1
Thomas Mathew <mothas.thomas@gmail.com>
2019-06-19 22:00:45 -0700

    Include pyspark dataframes in main.py

--- src/main.py
+++ src/main.py
@@ -5,16 +5,41 @@ import pretty_midi
 s3 = boto3.resource('s3')
 boto_client = boto3.client('s3')
 
-bucket = s3.Bucket('midi-files-partial')
+s3_bucket = 'midi-files-partial'
+bucket = s3.Bucket(s3_bucket)
 
 invalid_files = []
+number_of_files = 0
+number_of_valid_files = 0
+number_of_invalid_files = 0
+number_of_song_instruments = 0
 
+#Define Spark Configuration
+def spark_conf():
+    conf = SparkConf().setAppName("processMIDIfiles")
+    sc = SparkContext(conf=conf)
+    spark = SparkSession.builder.getOrCreate()
+    return spark
+
+Song_Instrument = Row("filename", "instrument")
+song_instrument_seq = []
+
+#Read all MIDI files from S3 bucket
 for obj in bucket.objects.all():
+    number_of_files+=1
     s3_key = obj.key
-    midi_obj_stream = boto_client.get_object(Bucket='midi-files-partial',Key=s3_key)
+    midi_obj_stream = boto_client.get_object(Bucket=s3_bucket, Key=s3_key)
+    midi_obj = BytesIO(midi_obj_stream['Body'].read())
     try:
-        midi_obj = BytesIO(midi_obj_stream['Body'].read())
         pretty_midi_obj = pretty_midi.PrettyMIDI(midi_obj)
-        print('Number of instruments: ', len(pretty_midi_obj.instruments))
+        number_of_song_instruments += len(pretty_midi_obj.instruments)
+        number_of_valid_files+=1
+        for ins in pretty_midi_obj.instruments:
+            song_instrument_seq.append(Song_Instrument(s3_key,ins))
     except:
         invalid_files.append(s3_key)
+        number_of_invalid_files+=1
+
+df_song_instrument = spark.createDataFrame(song_instrument_seq)
+
+print('!!! COUNT: ', df_song_instrument.count())
